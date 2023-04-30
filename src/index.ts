dab0ce40556d9317bca15a38d0d19462e2bfc726
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-04-30 00:36:04 -0500

    bubble sort algorithm implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello world!!!");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  //sorting function
+  sort(): void {
+    const { length } = this.collection;
+
+    //bubble sort algorithm
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          //swapping the value
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+//creating a object of the class
+const sorter = new Sorter([10, 5, -1, 2]);
+sorter.sort();
+
+console.log(sorter.collection);
